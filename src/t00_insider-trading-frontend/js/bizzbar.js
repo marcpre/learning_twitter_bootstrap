b31593f88ac185d061eefa9f9b0e68bbb808a1c0
marcpre <marcus.presich@gmail.com>
2021-07-30 06:56:21 +0200

    fixing hellobar

--- src/t00_insider-trading-frontend/js/bizzbar.js
+++ src/t00_insider-trading-frontend/js/bizzbar.js
@@ -10,17 +10,17 @@ $(document).ready(function() {
     bb_height += (2 * bb_padding) + bb_border;
 
     $("#bizzbar").effect("bounce", "slow");
-    $('header').css('margin-top', bb_height + 'px');
+    $('nav').css('margin-top', bb_height + 'px');
 
     $("open").click(function() {
         $("#bizzbar").effect("bounce", "slow");
         $("open").slideUp();
-        $('header').css('margin-top', bb_height + 'px');
+        $('nav').css('margin-top', bb_height + 'px');
     });
 
     $("close").click(function() {
         $("#bizzbar").slideUp();
         $("open").slideDown();
-        $('header').css('margin-top', '0px');
+        $('nav').css('margin-top', '0px');
     });
 });
